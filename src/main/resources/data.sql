85c4e31e0d4dc272c67fbe7b21e1003ec89baa48
Satria Aji Darmawan <satriaaji219@gmail.com>
2024-12-11 18:39:49 +0700

    tambah lagu

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
 INSERT INTO song (title, artist, file_path, duration) VALUES
 ('Wesley Theory', 'Kendrick Lamar', '/songs/wesley_theory.mp3', 225),
 ('Bohemian Rhapsody', 'Queen', '/songs/bohemian_rhapsody.mp3', 300),
-('Billie Jean', 'Michael Jackson', '/songs/billie_jean.mp3', 200);
+('Billie Jean', 'Michael Jackson', '/songs/billie_jean.mp3', 200),
+('Yowisben', 'Bayu Skak', '/songs/Yowisben.mp3', 200),
+('Yowisben', 'Bayu Skak', '/songs/Yowisben.mp3', 200),
+('Yowisben', 'Bayu Skak', '/songs/Yowisben.mp3', 200);
