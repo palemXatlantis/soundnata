4624acc0e65c495b9cd1158cb8c7c209e3033a42
netliar <sigithadi234@gmail.com>
2024-12-13 13:16:17 +0700

    fix typo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO song (title, artist, file_path, duration, image_path) VALUES
-('Wesley Theory', 'Kendrick Lamar', '/songs/wesley_theory.mp3', 225, '/images/albums/to_pimp_a_butterfly.png'),
+("Wesley's Theory", 'Kendrick Lamar', '/songs/wesley_theory.mp3', 225, '/images/albums/to_pimp_a_butterfly.png'),
 ('Bohemian Rhapsody', 'Queen', '/songs/bohemian_rhapsody.mp3', 300, '/images/albums/bohemian_rhapsody.jpg'),
 ('Billie Jean', 'Michael Jackson', '/songs/billie_jean.mp3', 200, '/images/albums/thriller.jpg'),
 ('Yowisben', 'Bayu Skak', '/songs/Yowisben.mp3', 200,'/images/albums/yowis_ben.jpg'),
