2100de89faa6fcef1b4ef8b956bc9c4f14087ddf
masFalah <134691882+falahkuv@users.noreply.github.com>
2024-12-14 21:00:26 +0700

    update lyrics ke db
    nyambungin ke db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT IGNORE INTO song (title, artist, file_path, duration, image_path, lyrics) VALUES
-('Telkom Remix', 'Lil Pung', '/songs/telkom_remix.mp3', 200,'/images/question_mark.jpg', ""),
-('Tentakel', 'Morgue Vanguard & Mardial', '/songs/tentakel.mp3', 200,'/images/albums/tentakel.jpg'),
-("Wesley's Theory", 'Kendrick Lamar', '/songs/wesley_theory.mp3', 225, '/images/albums/to_pimp_a_butterfly.png'),
-('Bohemian Rhapsody', 'Queen', '/songs/bohemian_rhapsody.mp3', 300, '/images/albums/bohemian_rhapsody.jpg'),
-('Billie Jean', 'Michael Jackson', '/songs/billie_jean.mp3', 200, '/images/albums/thriller.jpg'),
-('Yowisben', 'Bayu Skak', '/songs/Yowisben.mp3', 200,'/images/fight.jpg');
+('Telkom Remix', 'Lil Pung', '/songs/telkom_remix.mp3', 200,'/images/question_mark.jpg', "No Lyrics"),
+('Tentakel', 'Morgue Vanguard & Mardial', '/songs/tentakel.mp3', 200,'/images/albums/tentakel.jpg', ""),
+("Wesley's Theory", 'Kendrick Lamar', '/songs/wesley_theory.mp3', 225, '/images/albums/to_pimp_a_butterfly.png', ""),
+('Bohemian Rhapsody', 'Queen', '/songs/bohemian_rhapsody.mp3', 300, '/images/albums/bohemian_rhapsody.jpg', ""),
+('Billie Jean', 'Michael Jackson', '/songs/billie_jean.mp3', 200, '/images/albums/thriller.jpg', ""),
+('Yowisben', 'Bayu Skak', '/songs/Yowisben.mp3', 200,'/images/fight.jpg', "");
